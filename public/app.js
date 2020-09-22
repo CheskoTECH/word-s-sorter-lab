467a2e7444e9113d484dbeac4e439f3a72c278ea
CheskoTECH <cheskidov.peter@yandex.ru>
2020-09-22 11:07:04 +0500

    changes

--- public/app.js
+++ public/app.js
@@ -3,7 +3,9 @@ const messages = document.getElementById("messages");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
-const ws = new WebSocket("ws://localhost:5000");
+const PORT = process.env.PORT || "localhost:5000"; 
+
+const ws = new WebSocket(`ws://${PORT}`);
 
 function setStatus(value) {
   status.innerHTML = value;
